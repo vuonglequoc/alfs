8807ca35679bb40392c53d5186e5e9437a16b5ce
Vuong Le <lelordoftech@gmail.com>
2024-08-22 00:34:52 +0900

    Support download package with output filename which different with URL

--- scripts/get_packages.sh
+++ scripts/get_packages.sh
@@ -17,7 +17,20 @@ fi
 
 mkdir -v $LFS/sources
 
-wget --input-file=$LFS/alfs/wget-list --continue --directory-prefix=$LFS/sources
+# wget --input-file=$LFS/alfs/wget-list --continue --directory-prefix=$LFS/sources
+# Support download package with output filename which different with URL
+download() {
+  err=0
+  while read -r url filename; do
+    if [ ! -z $filename ]; then
+      wget --continue -O "$2/$filename" "$url" || err=1
+    else
+      wget --continue --directory-prefix="$2" "$url" || err=1
+    fi
+  done < $1
+  return $err
+}
+download $LFS/alfs/wget-list $LFS/sources
 
 pushd $LFS/sources
   md5sum -c $LFS/alfs/md5sums
